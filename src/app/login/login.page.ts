419fba083b408ed600d96ace1e4abea789ef7193
Phil <36074902+PhilippeBaumann@users.noreply.github.com>
2020-09-25 11:57:10 +0200

    Passing input Values

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -12,4 +12,19 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
+  signup(){
+    console.log(this.firstnameInput);
+    console.log(this.lastnameInput);
+    console.log(this.phoneInput);
+
+    // API request to create a new user
+
+    
+
+  }
+
+  login(){
+    console.log(this.tokenInput);
+  }
+
 }
