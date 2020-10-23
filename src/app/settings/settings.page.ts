5ca08944c8f8ab271bc49e5cfe5974a5b5375c35
Philippe Baumann <36074902+PhilCPNV@users.noreply.github.com>
2020-10-23 17:49:03 +0200

    Confirmation on logout

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -4,6 +4,7 @@ import { Storage } from '@ionic/storage';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../services/api.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -15,11 +16,38 @@ export class SettingsPage implements OnInit {
   // Try to access already initialized data or refresh it
   public data: DataCoreProvider;
   
+  // Advanced Toast
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Are you sure?',
+      message: 'all local data will be lost',
+      color: 'danger',
+      position: 'bottom',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'checkmark',
+          handler: () => {
+            this.logout()
+          }
+        }, {
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+  
   constructor(
     private router: Router,
     storage: Storage,
     private http: HttpClient,
     private apiService: ApiService,
+    private toastController: ToastController,
   ) {
     this.data = new DataCoreProvider(storage, http, apiService)
     this.data.init()
@@ -29,8 +57,11 @@ export class SettingsPage implements OnInit {
   }
 
   logout(){
-    this.data.deleteToken()
-    //this.data.deleteStorage()
+    //this.data.deleteToken()
+    this.data.deleteStorage()
+    // Clean the current view and set values
+    this.router.dispose()
+    // Show the Welcome page again
     this.router.navigate(['login'])
   }
 }
